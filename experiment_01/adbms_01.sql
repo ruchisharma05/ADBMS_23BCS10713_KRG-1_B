467e7f4150f6a32771b64d9ee07c2cd0e1c112e9
Ruchi Sharma <ruchi357073@gmail.com>
2025-07-30 19:04:57 +0530

    Add experiment_01 folder with PDF and SQL

---
+++ experiment_01/adbms_01.sql
@@ -0,0 +1,83 @@
+--EASY
+CREATE TABLE TBL_AUTHOR
+(
+	AUTHOR_ID INT PRIMARY KEY,
+	AUTHOR_NAME VARCHAR(10),
+	COUNTRY VARCHAR(10)
+)
+
+CREATE TABLE TBL_BOOK
+(
+	BOOK_ID INT PRIMARY KEY,
+	BOOK_TITLE VARCHAR(10),
+	AUHTORID INT
+	FOREIGN KEY (AUHTORID) REFERENCES TBL_AUTHOR(AUTHOR_ID)
+	--USED TO MAINTAIN REFERNTIAL INTEGRITY
+)
+
+INSERT INTO TBL_AUTHOR VALUES(101,'A','INDIA');
+INSERT INTO TBL_AUTHOR VALUES(103,'B','JAPAN');
+INSERT INTO TBL_BOOK VALUES(121,'ABC',103);
+INSERT INTO TBL_BOOK VALUES(111,'XYZ',101);
+
+SELECT B.BOOK_TITLE, A.AUTHOR_NAME AS 'AUTHOR NAME' , A.COUNTRY --[AUTHOR NAME] ->changes column name
+FROM TBL_BOOK AS B
+INNER JOIN
+TBL_AUTHOR AS A 
+ON
+B.AUHTORID = A.AUTHOR_ID
+
+
+
+
+--MEDIUM
+CREATE TABLE Department (
+    DeptID INT PRIMARY KEY,
+    DeptName VARCHAR(100)
+);
+
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100),
+    DeptID INT,
+    FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
+);
+
+INSERT INTO Department VALUES
+(1, 'Computer Science'),
+(2, 'Physics'),
+(3, 'Mathematics'),
+(4, 'Chemistry'),
+(5, 'Biology');
+
+INSERT INTO Course VALUES
+(101, 'Data Structures', 1),
+(102, 'Operating Systems', 1),
+(103, 'Quantum Mechanics', 2),
+(104, 'Electromagnetism', 2),
+(105, 'Linear Algebra', 3),
+(106, 'Calculus', 3),
+(107, 'Organic Chemistry', 4),
+(108, 'Physical Chemistry', 4),
+(109, 'Genetics', 5),
+(110, 'Molecular Biology', 5);
+
+INSERT INTO Course VALUES(111, 'DBMS', 2);
+
+SELECT DeptName
+FROM Department
+WHERE DeptID IN (
+    SELECT DeptID
+    FROM Course
+    GROUP BY DeptID
+    HAVING COUNT(*) > 2
+);
+
+
+CREATE LOGIN LOGIN_ABC
+WITH PASSWORD = 'ABC@04';
+
+
+CREATE USER ABC_04 FOR LOGIN LOGIN_ABC;
+
+GRANT SELECT ON Course TO ABC_04;
